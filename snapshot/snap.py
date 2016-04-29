c9fd68de6d2bd771d65e57b002b93a52316cbec8
fjctp <snchan20@yahoo.com>
2016-04-29 15:14:53 -0700

    fixed UI responsiveness bug

--- snapshot/snap.py
+++ snapshot/snap.py
@@ -1,22 +1,28 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
-i = 1
+CAMERA_ID = 0
+
+cap = cv2.VideoCapture(CAMERA_ID)
 #out = cv2.VideoWriter('test.avi', cv2.cv.CV_FOURCC(*'XVID'), 20.0, (640, 480))
 
+print('Press \'s\' to save image')
+print('Press \'q\' to quit')
+
+i = 1
 while(cap.isOpened()):
   ret, frame = cap.read()
   if ret:
     cv2.imshow('frame', frame)
     #out.write(frame)
     
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    k = cv2.waitKey(1)
+    if k == ord('q'):
       print('Closing')
       break
-    if cv2.waitKey(1) & 0xFF == ord('s'):
+    elif k == ord('s'):
       filename = str(i) + '.png'
-      print('Savingi to ' + filename)
+      print('Saving to ' + filename)
       cv2.imwrite(filename, frame)
       i = i + 1
   else:
